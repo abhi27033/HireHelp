b4c7c2bc99f6b0a85bdf2cf0f3e9ba46e1a16e12
ArYTaN003 <aryan.tandon03@gmail.com>
2024-08-21 03:58:25 +0530

    Added Home and candidate Page

--- recruitment_app/urls.py
+++ recruitment_app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.abc, name='abc'),
-    path('recruiter', views.rec, name='def'),
+    path('', views.index, name='index'),
+    path('candidate',views.candidate,name='candidate'),
+    path('interviewer',views.interviewer,name='interviewer')
 ]
