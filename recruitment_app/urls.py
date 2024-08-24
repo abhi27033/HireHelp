79c0fb0ea9acaed8b1590088b749296a744f28a4
atharva_singh <athsingh3123@gmail.com>
2024-08-24 16:02:18 +0530

    added auth and login

--- recruitment_app/urls.py
+++ recruitment_app/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login',views.login_view,name='.login_view'),
     path('candidate',views.candidate,name='candidate'),
     path('interviewer',views.interviewer,name='interviewer'),
     path('register',views.register,name='register')
