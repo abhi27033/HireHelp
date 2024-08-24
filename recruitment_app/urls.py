31887e1ba9ce2ed6bc41e41e24072a7e7603cf28
Abhishek Singh <abhishek27033@gmail.com>
2024-08-24 19:12:40 +0530

    Added logout button and resoved the sid bug in case of duplicate mob or email

--- recruitment_app/urls.py
+++ recruitment_app/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('login',views.login_view,name='.login_view'),
     path('candidate',views.candidate,name='candidate'),
     path('interviewer',views.interviewer,name='interviewer'),
-    path('register',views.register,name='register')
+    path('register',views.register,name='register'),
+    path('logout', views.logout_view, name='logout')
 ]
