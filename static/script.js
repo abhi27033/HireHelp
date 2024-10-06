ab277ef59b83fa077e07d5e576322a7ee2f904d1
atharva_singh <athsingh3123@gmail.com>
2024-10-07 00:46:45 +0530

    signup page changes

--- static/script.js
+++ static/script.js
@@ -9,3 +9,18 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
+/*Typing animation*/
+function animate_text(){
+	var txt = document.getElementById("animate-letter-typing1").innerHTML;
+	document.getElementById("animate-letter-typing1").innerHTML = "";
+	typeWriter(txt,"animate-letter-typing1");
+	var txt = document.getElementById("animate-letter-typing2").innerHTML;
+	document.getElementById("animate-letter-typing2").innerHTML = "";
+	typeWriter(txt,"animate-letter-typing2");
+}
+function typeWriter(txt,class_name) {
+	if (txt.length>0) {
+	  document.getElementById(class_name).innerHTML += txt.charAt(0);
+	  setTimeout(typeWriter, 100,txt.substr(1),class_name);
+	}
+}
