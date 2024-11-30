75599589c86145e0b2e42fd666ab4fe5128e92b3
atharva_singh <athsingh3123@gmail.com>
2024-12-01 05:22:03 +0530

    late night commit v2

--- recruitment_app/test.py
+++
@@ -1,6 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyD_JUtNlEQcjjSp9XlSrQKAy4OMBRl9N5g")
-model = genai.GenerativeModel("gemini-1.5-flash")
-response = model.generate_content("Explain how AI works")
-print(response.text)
